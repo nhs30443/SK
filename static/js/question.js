e92f6b68cde82a83894b28711c8d589ca7bc5734
BoB-bo2 <desupi001@docomo.ne.jp>
2025-05-27 10:20:05 +0900

    questionjs

---
+++ static/js/question.js
@@ -0,0 +1,6 @@
+// 例：選択肢をクリックしたら表示する
+document.querySelectorAll('.choice').forEach(choice => {
+    choice.addEventListener('click', () => {
+    alert(`「${choice.textContent}」が選ばれました`);
+    });
+});
