2d0cd804be16cf741aa324c7ccdf3c4ac63d5ed9
BoB-bo2 <desupi001@docomo.ne.jp>
2025-05-27 10:50:47 +0900

    qustion

--- static/js/question.js
+++ static/js/question.js
@@ -9,5 +9,15 @@ $(function(){
   $('#hamburger').on('click',function(){
     $('#hamburger').toggleClass("open");
     $('#header-menu').slideToggle();
+    $('#overlay').fadeToggle(); // オーバーレイ表示切り替え
+  });
+});
+
+
+  // オーバーレイをクリックしたらメニューを閉じる処理も追加
+  $('#overlay').on('click', function() {
+    $('#hamburger').removeClass("open");
+    $('#header-menu').slideUp();
+    $('#overlay').fadeOut();
   });
 });
